be43da33841439a23734103c2ca6c986698ec002
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2015-05-08 10:54:33 -0500

    right query for the new DB scheme

--- DB/test_targetting.py
+++ DB/test_targetting.py
@@ -23,30 +23,18 @@ def get_tiles():
         raise e    
     return ra_desi, dec_desi, pass_desi
 
-#get the tiles in desi
-ra_tiles, dec_tiles, pass_tiles = get_tiles()
 
-#open database connection
-con = psycopg2.connect(host='scidb2.nersc.gov', user='desi_user', database='desi')
-cur = con.cursor()
-
-#file to write the data
-out = open("../data/decals_ngals.dat", "w")
-out.write("# number of decals sources in a circle of 0.7 deg in radius\n")
-out.write("# index, ra , dec , ipass , ngaln\n")
-out.close()
-
-#loop over tiles to count the number of galaxies inside
-index = 0
-n_tiles = np.size(pass_tiles)
-for i in range(10):
-    cur.execute("select id, ra, dec from candidate where q3c_radial_query(candidate.ra, candidate.dec, %f, %f,0.7);"%(ra_tiles[i],dec_tiles[i]))
+def simple_test():
+    ra = 335.0
+    dec = 0.0
+    radius = 0.7
+    con = psycopg2.connect(host='scidb2.nersc.gov', user='desi_user', database='desi')
+    cur = con.cursor()
+    cur.execute("select candidate.id, candidate.ra, candidate.dec, decam.gflux, decam.rflux, decam.zflux, wise.w1flux, decam.gfracflux, decam.rfracflux, decam.zfracflux, wise.w1fracflux from  candidate, decam, wise where q3c_radial_query(candidate.ra, candidate.dec, %f, %f, %f) and decam.cand_id = candidate.id and wise.cand_id = candidate.id and decam.g_anymask=0 and decam.r_anymask =0 and decam.z_anymask=0;"%(ra,dec,radius))
     m=cur.fetchall()
-    print "looking for in", i
-    if(len(m)):
-        out = open("../data/decals_ngals.dat", "a")
-        print "something in", i
-        out.write("%d %f %f %d %d\n"%(i, ra_tiles[i], dec_tiles[i], pass_tiles[i], len(m)))
-        out.close()
-if con:
-    con.close()
+    data = np.array(m)
+
+
+    if con:
+        con.close()
+        
