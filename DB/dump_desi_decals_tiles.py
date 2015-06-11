2ffd071338f1cfcefb00ad3a92d4a508bbb1e705
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2015-06-10 22:01:21 -0700

    first version of the script extracting actual decals on desi footprint

--- DB/dump_desi_decals_tiles.py
+++ DB/dump_desi_decals_tiles.py
@@ -1,4 +1,41 @@
+import psycopg2
+import sys
+import numpy as np
+import fitsio as F
 import test_targetting as tt
+from astropy.io import fits
+import os
 
+def get_tiles():
+    """
+    input: none
+    output: numpy array with RA, DEC, PASS footprint information from desi-tiles.fits
+    """
+    tiling_file = os.path.join(os.environ['DESIMODEL'], 'data/footprint/', 'desi-tiles.fits')
+    try:
+        dat = F.read(tiling_file,columns=('RA','DEC','PASS','IN_DESI', 'TILEID'))
+        ww  = np.nonzero( dat['IN_DESI'] )[0]
+        ra_desi  = (dat[  'RA'][ww]).astype('f4')
+        dec_desi  = (dat[ 'DEC'][ww]).astype('f4')
+        pass_desi = (dat[ 'PASS'][ww]).astype('int')
+        tileid_desi = (dat[ 'TILEID'][ww]).astype('int')
+    except Exception, e:
+        import traceback
+        print 'ERROR in get_tiles'
+        traceback.print_exc()
+        raise e    
+    return ra_desi, dec_desi, pass_desi, tileid_desi
 
-tt.extract_targets(ra=334.0, dec=0.0, radius=1.6, tile_id=1)
+ra_tiles, dec_tiles, pass_tiles, tileid = get_tiles()
+
+id_decals = np.where((ra_tiles<360.0)&(ra_tiles>320.0)&(dec_tiles<0.5)&(dec_tiles>-0.5))
+id_decals = id_decals[0]
+print dec_tiles[id_decals], ra_tiles[id_decals], pass_tiles[id_decals], tileid[id_decals]
+
+n_tiles = np.size(id_decals)
+for i in range(0,n_tiles):
+    ra = ra_tiles[id_decals[i]]
+    dec = dec_tiles[id_decals[i]]
+    tile = tileid[id_decals[i]]
+    print "working on ra %f dec %f, item %d"%(ra,dec, i)
+    tt.extract_targets(ra=ra, dec=dec, radius=1.6, tile_id=tile)
