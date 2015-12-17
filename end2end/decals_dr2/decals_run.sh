cab7e81a97ec721c241c1bc04a40269f3b3c3631
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2015-12-17 14:52:27 -0800

    quick cat

--- end2end/decals_dr2/decals_run.sh
+++ end2end/decals_dr2/decals_run.sh
@@ -4,3 +4,5 @@ cd ~/mtl/bin
 ./build_mtl /global/cscratch1/sd/forero/targetdata/decals_targets.fits /global/cscratch1/sd/forero/specdata/spec1.fits /global/cscratch1/sd/forero/mtldata/mtl1.fits
 cd ~/fiberassign/
 ./bin/assign /global/homes/f/forero/SandboxDESI/end2end/decals_dr2/inputs/fiberassign_pass_1.txt 
+cd ~/desisim/bin/
+./quickcat -f /global/cscratch1/sd/forero/fiberdata/pass_1/ -t //global/homes/f/forero/SandboxDESI/end2end/decals_dr2/inputs/tile_list_pass_1.txt -T dumb.fits -z /global/cscratch1/sd/forero/specdata/spec1.fits -o  /global/cscratch1/sd/forero/specdata/spec2.fits
