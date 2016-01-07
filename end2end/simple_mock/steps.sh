de1adafdb913dd7e5d1ec9d067bba92713f6d47a
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2016-01-06 16:15:54 -0800

    first steps end2end for the simple mock

---
+++ end2end/simple_mock/steps.sh
@@ -0,0 +1,5 @@
+./bin/./assign /global/homes/f/forero/SandboxDESI/end2end/simple_mock/inputs/fiberassign_pass_0.txt 
+
+./quickcat -f /global/cscratch1/sd/forero/simplemock_end2end/fiberdata/pass_0/ -t /global/cscratch1/sd/forero/simplemock_end2end/layerdata/dark_epoch0.txt -T /global/cscratch1/sd/forero/simplemock_end2end/mtldata/truth_objects_ss_sf0.fits -o /global/cscratch1/sd/forero/simplemock_end2end/quickcatdata/zcat_pass_0.fits
+
+./build_mtl -i /global/cscratch1/sd/forero/simplemock_end2end/mtldata/objects_ss_sf0.fits -z /global/cscratch1/sd/forero/simplemock_end2end/quickcatdata/zcat_pass_0.fits  -o  /global/cscratch1/sd/forero/simplemock_end2end/mtldata/mtl_pass_1.fits
