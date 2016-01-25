6bbe5dd6a41ada6a8690a2263d044162da8af065
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2016-01-25 05:27:29 -0800

    command and input for first pass of fiberassign

--- end2end/simple_mock/steps.sh
+++ end2end/simple_mock/steps.sh
@@ -1,4 +1,4 @@
-./bin/./assign /global/homes/f/forero/SandboxDESI/end2end/simple_mock/inputs/fiberassign_pass_0.txt 
+./bin/./fiberassign /global/homes/f/forero/SandboxDESI/end2end/simple_mock/inputs/fiberassign_pass_0.txt 
 
 ./quickcat -f /global/cscratch1/sd/forero/simplemock_end2end/fiberdata/pass_0/ -t /global/cscratch1/sd/forero/simplemock_end2end/layerdata/dark_epoch0.txt -T /global/cscratch1/sd/forero/simplemock_end2end/mtldata/truth_objects_ss_sf0.fits -o /global/cscratch1/sd/forero/simplemock_end2end/quickcatdata/zcat_pass_0.fits
 
