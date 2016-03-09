e14b944d2c9731c6ea62730e819cebb44b101265
forero <j.e.forero.romero@gmail.com>
2016-03-09 10:48:29 -0500

    new test

--- PR_test/new_test.py
+++ PR_test/new_test.py
@@ -43,10 +43,11 @@ tilefiles = sorted(glob(tiledir+'/tile*.fits'))
 len(tilefiles)
 
 # this ugly hack avoids import from desi stack
-import os
-os.chdir('/global/homes/f/forero/desisim/py')
+desisim = imp.load_source("desisim.quickcat", "/global/homes/f/forero/desisim/py/desisim/quickcat.py")
+#import os
+#os.chdir('/global/homes/f/forero/desisim/py')
 
 #testing this PR: https://github.com/desihub/desisim/pull/85
-from desisim.quickcat import quickcat
-zcat = quickcat(tilefiles, targets, truth, zcat=None, perfect=False)
+#from desisim.quickcat import quickcat
+zcat = desisim.quickcat(tilefiles, targets, truth, zcat=None, perfect=False)
 zcat.write('zcat.fits', overwrite=True)
