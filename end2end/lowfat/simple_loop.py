b48066eba17406ed99ff07edb64160ccb024f8f5
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2016-03-14 16:58:05 -0700

    simplified inputs

--- end2end/lowfat/simple_loop.py
+++ end2end/lowfat/simple_loop.py
@@ -2,12 +2,12 @@ import numpy as np
 import desitarget.mtl
 import os
 import shutil
-from desisim import quickcat
+from desisim.quickcat import quickcat
 import glob
 import subprocess
 from astropy.table import Table, Column
 
-def assign_quickcat_mtl_loop(destination_path=None, epoch_id = 0, targets_path=None, zcat_file=None, fiberassign_exec=None):
+def mtl_assign_quickcat_loop(destination_path=None, targets_path=None, zcat_file=None, fiberassign_exec=None, epoch_id=0):
 
     truth = Table.read(os.path.join(targets_path,'truth.fits'))
     targets = Table.read(os.path.join(targets_path,'targets.fits'))
@@ -19,27 +19,36 @@ def assign_quickcat_mtl_loop(destination_path=None, epoch_id = 0, targets_path=N
         zcat = Table.read(zcat_file, format='fits')
         mtl = desitarget.mtl.make_mtl(targets, zcat)
         mtl.write(os.path.join(destination_path, 'mtl.fits'), overwrite=True)
+    print("Finished MTL")
 
-#    p = subprocess.call([fiberassign_exec, fiberassign_features], stdout=subprocess.PIPE)
-    p = subprocess.call([fiberassign_exec, os.path.join(destination_path, 'fa_features.txt')])
-
-    #find the tiles
+    # clean fits files before fiberassing
     fiberassign_output_path = os.path.join(destination_path, 'fiberassign/')
+    tilefiles = sorted(glob.glob(fiberassign_output_path+'/tile*.fits'))
+    if tilefiles:
+        for tilefile in tilefiles:
+            os.remove(tilefile)
+
+    # launch fiberassign
+    p = subprocess.call([fiberassign_exec, os.path.join(destination_path, 'fa_features.txt')], stdout=subprocess.PIPE)
+    print("Finished fiberassign")
+
+    #find the output fibermap tiles
     if not os.path.exists(fiberassign_output_path):
             os.makedirs(fiberassign_output_path)
     tilefiles = sorted(glob.glob(fiberassign_output_path+'/tile*.fits'))
     print("{} tiles in fiberassign output".format(len(tilefiles)))
 
-    #from desisim.quickcat import quickcat
+    # write the zcat
     if zcat_file is None:
         zcat_file = os.path.join(destination_path, 'zcat.fits')
-        zcat = quickcat.quickcat(tilefiles, targets, truth, zcat=None, perfect=False)
+        zcat = quickcat(tilefiles, targets, truth, zcat=None, perfect=False)
         zcat.write(zcat_file, overwrite=True)
     else:
         zcat_file = os.path.join(destination_path, 'zcat.fits')
         zcat = Table.read(zcat_file, format='fits')
-        newzcat = quickcat.quickcat(tilefiles, targets, truth, zcat=zcat, perfect=False)
+        newzcat = quickcat(tilefiles, targets, truth, zcat=zcat, perfect=False)
         newzcat.write(zcat_file, format='fits', overwrite=True)
+    print("Finished zcat")
 
     # keep a copy of zcat.fits 
     tmp_output_path = os.path.join(destination_path, '{}'.format(epoch_id))
@@ -47,9 +56,6 @@ def assign_quickcat_mtl_loop(destination_path=None, epoch_id = 0, targets_path=N
             os.makedirs(tmp_output_path)
     shutil.copy(zcat_file, tmp_output_path)
 
-    #clean files
-    for tilefile in tilefiles:
-        os.remove(tilefile)
     return zcat_file
 
 destination_path =  "/project/projectdirs/desi/users/forero/lowfat/"
@@ -66,14 +72,13 @@ fx = open(os.path.join(destination_path, 'fa_features.txt'), 'w')
 fx.write(params.format(inputdir=destination_path, targetdir=targets_path))
 fx.close()
 
-
 n_epochs = 5
 zcat_file = None
 for i in range(n_epochs):
     epochfile = os.path.join(epoch_path, "epoch{}.txt".format(i))
     shutil.copy(epochfile, os.path.join(destination_path, "survey_list.txt"))
                 
-    zcat_file = assign_quickcat_mtl_loop(
-        destination_path = destination_path, epoch_id = i, targets_path = targets_path,
-        zcat_file = zcat_file, fiberassign_exec = fiberassign_exec)
+    zcat_file = mtl_assign_quickcat_loop(
+        destination_path = destination_path, targets_path = targets_path,
+        zcat_file = zcat_file, fiberassign_exec = fiberassign_exec, epoch_id = i)
 
