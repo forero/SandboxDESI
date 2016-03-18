843e3860fc964e1c206d629a1640dcd15c449afc
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2016-03-18 10:53:56 -0700

    added perfect=False option

--- end2end/lowfat/simple_loop.py
+++ end2end/lowfat/simple_loop.py
@@ -9,7 +9,7 @@ from astropy.table import Table, Column
 import os.path
 
 def mtl_assign_quickcat_loop(output_path=None, targets_path=None, zcat_file=None, fiberassign_exec=None, epoch_path=None, 
-                             epoch_id=0, fiber_epochs=[], mtl_epochs=[]):
+                             epoch_id=0, fiber_epochs=[], mtl_epochs=[], perfect=True):
 
     # create temporary output paths
     tmp_output_path = os.path.join(output_path, 'tmp/')
@@ -36,11 +36,11 @@ def mtl_assign_quickcat_loop(output_path=None, targets_path=None, zcat_file=None
     targets = Table.read(os.path.join(targets_path,'targets.fits'))
     
     if zcat_file is None:
-        mtl = desitarget.mtl.make_mtl(targets, trim=False)
+        mtl = desitarget.mtl.make_mtl(targets)
         mtl.write(os.path.join(tmp_output_path, 'mtl.fits'), overwrite=True)
     else:
         zcat = Table.read(zcat_file, format='fits')
-        mtl = desitarget.mtl.make_mtl(targets, zcat, trim=False)
+        mtl = desitarget.mtl.make_mtl(targets, zcat)
         mtl.write(os.path.join(tmp_output_path, 'mtl.fits'), overwrite=True)
     print("Finished MTL")
 
@@ -77,12 +77,12 @@ def mtl_assign_quickcat_loop(output_path=None, targets_path=None, zcat_file=None
     # write the zcat
     if zcat_file is None:
         zcat_file = os.path.join(tmp_output_path, 'zcat.fits')
-        zcat = quickcat(tilefiles, targets, truth, zcat=None, perfect=True)
+        zcat = quickcat(tilefiles, targets, truth, zcat=None, perfect=perfect)
         zcat.write(zcat_file, overwrite=True)
     else:
         zcat_file = os.path.join(tmp_output_path, 'zcat.fits')
         zcat = Table.read(zcat_file, format='fits')
-        newzcat = quickcat(tilefiles, targets, truth, zcat=zcat, perfect=True)
+        newzcat = quickcat(tilefiles, targets, truth, zcat=zcat, perfect=perfect)
         newzcat.write(zcat_file, format='fits', overwrite=True)
     print("Finished zcat")
 
@@ -115,7 +115,7 @@ fiberassign_exec = "/global/homes/f/forero/fiberassign/bin/./fiberassign"
 
 
 # new kind of setup
-output_path =  "/project/projectdirs/desi/users/forero/lowfat_perfect/"
+output_path =  "/project/projectdirs/desi/users/forero/lowfat/"
 if not os.path.exists(output_path):
     os.makedirs(output_path)
 
@@ -141,7 +141,7 @@ for i in range(n_epochs):
         output_path = output_path, targets_path = targets_path,
         zcat_file = zcat_file, fiberassign_exec = fiberassign_exec, epoch_path = epoch_path,
         epoch_id = i, 
-        fiber_epochs = [i], mtl_epochs = range(i,n_epochs))
+        fiber_epochs = [i], mtl_epochs = range(i,n_epochs), perfect=False)
 
 # run everything on a single epoch
 zcat_file = None
@@ -149,51 +149,8 @@ zcat_file = mtl_assign_quickcat_loop(
     output_path = output_path, targets_path = targets_path,
     zcat_file = zcat_file, fiberassign_exec = fiberassign_exec, epoch_path = epoch_path,
     epoch_id = n_epochs, 
-    fiber_epochs = range(n_epochs), mtl_epochs = range(n_epochs))
+    fiber_epochs = range(n_epochs), mtl_epochs = range(n_epochs), perfect=False)
 
 #clean up tmp directory
 if os.path.exists(tmp_output_path):
     shutil.rmtree(tmp_output_path)
-
-#new kind of setup
-output_path =  "/project/projectdirs/desi/users/forero/lowfat_serial_perfect/"
-if not os.path.exists(output_path):
-    os.makedirs(output_path)
-
-tmp_output_path = os.path.join(output_path, 'tmp/')
-if not os.path.exists(tmp_output_path):
-    os.makedirs(tmp_output_path)
-
-tmp_fiber_path = os.path.join(tmp_output_path, 'fiberassign/')
-if not os.path.exists(tmp_fiber_path):
-    os.makedirs(tmp_fiber_path)
-
-# create temporaty input file for fiberassign
-params = ''.join(open('template_fiberassign.txt').readlines())
-fx = open(os.path.join(tmp_output_path, 'fa_features.txt'), 'w')
-fx.write(params.format(inputdir=tmp_output_path, targetdir=targets_path))
-fx.close()
-
-# loop over the epochs
-n_epochs = 5
-#zcat_file = None
-#for i in range(n_epochs):
-#    zcat_file = mtl_assign_quickcat_loop( 
-#        output_path = output_path, targets_path = targets_path,
-#        zcat_file = zcat_file, fiberassign_exec = fiberassign_exec, epoch_path = epoch_path,
-#        epoch_id = i, 
-#        fiber_epochs = [i], mtl_epochs = [i])
-
-
-# run everything on a single epoch
-#zcat_file = None
-#zcat_file = mtl_assign_quickcat_loop( 
-#    output_path = output_path, targets_path = targets_path,
-#    zcat_file = zcat_file, fiberassign_exec = fiberassign_exec, epoch_path = epoch_path,
-#    epoch_id = n_epochs, 
-#    fiber_epochs = range(n_epochs), mtl_epochs = range(n_epochs))
-
-#clean up tmp directory
-#if os.path.exists(tmp_output_path):
-#    shutil.rmtree(tmp_output_path)
-
