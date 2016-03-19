cf11fa4d128e2bf39f82dd5bc751b8e726648e11
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2016-03-18 17:35:20 -0700

    n_epochs as input

--- end2end/quicksurvey/qs_test.py
+++ end2end/quicksurvey/qs_test.py
@@ -5,13 +5,13 @@ targets_path = '/project/projectdirs/desi/mocks/preliminary/mtl/lowfat/'
 fiberassign_exec = '/global/homes/f/forero/fiberassign/bin/./fiberassign'
 epochs_path = '/project/projectdirs/desi/mocks/preliminary/mtl/lowfat/'
 template_fiberassign = 'template_fiberassign.txt'
-epochs_list = range(5)
+n_epochs = 5
 
 setup = qs.SimSetup(output_path = output_path, 
-                      targets_path = targets_path, 
-                      fiberassign_exec = fiberassign_exec, 
-                      epochs_path = epochs_path, 
-                      template_fiberassign = template_fiberassign, 
-                      epochs_list = epochs_list)
+                    targets_path = targets_path, 
+                    fiberassign_exec = fiberassign_exec, 
+                    epochs_path = epochs_path, 
+                    template_fiberassign = template_fiberassign, 
+                    n_epochs = n_epochs)
 
 qs.simulate_setup(setup)
