a849baebf3b98ec9b5b5530d13432815737f831b
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2016-03-18 18:49:31 -0700

    simple stats

---
+++ end2end/quicksurvey/qs_stats.py
@@ -0,0 +1,18 @@
+import desisim.quicksurvey as qs
+
+output_path = '/project/projectdirs/desi/users/forero/lowfat_lib/'
+targets_path = '/project/projectdirs/desi/mocks/preliminary/mtl/lowfat/'
+fiberassign_exec = '/global/homes/f/forero/fiberassign/bin/./fiberassign'
+epochs_path = '/project/projectdirs/desi/mocks/preliminary/mtl/lowfat/'
+template_fiberassign = 'template_fiberassign.txt'
+n_epochs = 5
+
+setup = qs.SimSetup(output_path = output_path, 
+                    targets_path = targets_path, 
+                    fiberassign_exec = fiberassign_exec, 
+                    epochs_path = epochs_path, 
+                    template_fiberassign = template_fiberassign, 
+                    n_epochs = n_epochs)
+
+qs.summary_setup(setup)
+
