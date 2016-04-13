7dc6e2e1da79bea270240b4e18b0b7968d228e2d
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2016-04-13 08:45:53 -0700

    udpated quicksurvey stats, python and shell script

--- end2end/quicksurvey/quick_survey.sh
+++ end2end/quicksurvey/quick_survey.sh
@@ -1,1 +1,3 @@
 python quicksurvey_run.py -O /project/projectdirs/desi/users/forero/lowfat_lib/ -T /project/projectdirs/desi/mocks/preliminary/mtl/lowfat/ -f /global/homes/f/forero/fiberassign/bin/./fiberassign -E /project/projectdirs/desi/mocks/preliminary/mtl/lowfat/ -t template_fiberassign.txt -N 5
+
+python quicksurvey_stats.py -O /project/projectdirs/desi/users/forero/lowfat_lib/ -T /project/projectdirs/desi/mocks/preliminary/mtl/lowfat/ -f /global/homes/f/forero/fiberassign/bin/./fiberassign -E /project/projectdirs/desi/mocks/preliminary/mtl/lowfat/ -t template_fiberassign.txt -N 5
