469b79c3d0c5cfd236b5054def7f4b68a74501a9
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2016-04-13 08:15:15 -0700

    quick survey shell script

---
+++ end2end/quicksurvey/quick_survey.sh
@@ -0,0 +1,1 @@
+python quicksurvey_run.py -O /project/projectdirs/desi/users/forero/lowfat_lib/ -T /project/projectdirs/desi/mocks/preliminary/mtl/lowfat/ -f /global/homes/f/forero/fiberassign/bin/./fiberassign -E /project/projectdirs/desi/mocks/preliminary/mtl/lowfat/ -t template_fiberassign.txt -N 5
