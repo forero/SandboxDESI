7c66d32ba951639b4eadeabe1260885088c9816a
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2017-03-07 07:53:54 -0800

    write join

--- end2end/desiflow/join_truth_targets.py
+++ end2end/desiflow/join_truth_targets.py
@@ -1,21 +1,27 @@
 import fitsio
 import os
 import desitarget.io as dtio
-from astropy.table import Table
+from astropy.table import Table, vstack
 
 root = '/global/project/projectdirs/desi/users/forero/datachallenge2017/test_201703/test/'
 iter_truth = dtio.iter_files(root, 'truth')
 iter_target = dtio.iter_files(root, 'targets')
 
-n = 0
+alltruth = []
+alltargets = []
 for truth_file, target_file in zip(iter_truth, iter_target):
-    truth = Table.read(truth_file)
-    targets = Table.read(target_file)
+    alltruth.append(Table.read(truth_file))
+    alltargets.append(Table.read(target_file))
     print(truth_file, target_file)
 
-#path = '/global/project/projectdirs/desi/users/forero/datachallenge2017/test_201703/test/209'
-#truth_filename = os.path.join(path, 'truth-2090m020.fits')
-#file = fitsio.FITS(truth_filename)
-#wave = file['WAVE'].read()
-#flux = file['FLUX'].read()
-#truth = file['FLUX'].read()
+
+targets = vstack(alltargets)
+truth = vstack(alltruth)
+
+out_targets = os.path.join(root,'targets.fits')
+out_truth = os.path.join(root,'truth.fits')
+targets.write(out_targets)
+truth.write(out_truth)
+
+print(truth)
+print(targets)
