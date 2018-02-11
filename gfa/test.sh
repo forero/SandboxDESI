ba8c9f98ed3ee71d364138bc17a332f0bdcf5c20
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2018-02-11 11:01:19 -0800

    test command

---
+++ gfa/test.sh
@@ -0,0 +1,2 @@
+# test command
+python select_gfa.py -o $SCRATCH/gfa_test/ -s /project/projectdirs/cosmo/data/legacysurvey/dr5/sweep/5.0/ -f /global/cscratch1/sd/forero/DR5FiberAssign/fiberassign/tileresults/ -t gfa_tiles_all.txt
