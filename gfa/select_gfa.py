f08dc1bd6eec472a2efc07533a61f9cbaf4f5359
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2018-02-11 10:42:28 -0800

    scrupt

--- gfa/select_gfa.py
+++ gfa/select_gfa.py
@@ -24,10 +24,30 @@ print(len(fiberassign_tilefiles))
 
 # list sweep files
 sweep_files = desitarget.io.list_sweepfiles(args.input_sweep)
+n_sweep = len(sweep_files)
 print(len(sweep_files))
 
 # list of tiles to be computed
-gfa_tiles = np.loadtxt(args.input_tiles)
+gfa_tiles = np.int_(np.loadtxt(args.input_tiles))
 print(len(gfa_tiles))
 
+#load all sweep data
+sweep_data = []
+n_sweep = 10
+for i in range(n_sweep):
+    sweep_file = sweep_files[i]
+    sweep_data.append(fitsio.read(sweep_file, columns=['RA', 'DEC', 'FLUX_R']))
+all_sweep = np.concatenate(sweep_data, axis=0)
 
+print('There are {:.2f}M targets in the sweeps'.format(len(all_sweep)/1E6))
+
+desi_tiles = desimodel.io.load_tiles()
+
+#find IDs of targets on every individual tile
+all_targetindices = dict()
+for tile_id in gfa_tiles:
+    ii = desi_tiles['TILEID'] == tile_id
+    print('computing TILEID {:05d} on RA {} DEC {}'.format(tile_id, desi_tiles['RA'][ii], desi_tiles['DEC'][ii]))
+    targetindices, gfaindices = desimodel.focalplane.on_tile_gfa(tile_id, all_sweep)
+    all_targetindices[tile_id] = targetindices
+    print('Found {:05d} targets on TILEID {:d}'.format(len(targetindices), tile_id))
