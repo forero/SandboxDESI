58b38f91b43f870a0da452ec55202f9df254e829
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2020-03-26 18:36:37 -0700

    produce separate footprint

--- DR8/join_DR8.py
+++ DR8/join_DR8.py
@@ -30,7 +30,7 @@ def write_initial_mtl_files(output_path="./"):
     data.close()
     
     # Read all the other files
-    for i, i_name in enumerate(target_files[25:]): #FIXME - should start at 1
+    for i, i_name in enumerate(target_files[1:]): 
         data = fitsio.FITS(i_name, 'r')
         tmp_data = data[1].read(columns=['TARGETID', 'DESI_TARGET', 'MWS_TARGET', 'BGS_TARGET', 'SUBPRIORITY', 'NUMOBS_INIT', 'PRIORITY_INIT', 'RA', 'DEC', 'HPXPIXEL', 'BRICKNAME'])
         target_data = np.hstack((target_data, tmp_data))
@@ -78,6 +78,64 @@ def write_initial_mtl_files(output_path="./"):
         full_mtl[~(ii_mtl_dark | ii_mtl_gray) & ~ii_north].write(mtl_file, overwrite=True)
         print("Wrote output to {}".format(mtl_file))
 
+def split_sky(output_path="./"):
+    # Create output directory
+    os.makedirs(output_path, exist_ok=True)
+    
+    sky_data = Table.read("/global/cfs/cdirs/desi/target/catalogs/dr8/0.31.0/skies/skies-dr8-0.31.0.fits")
+    ii_north = (sky_data['RA']>85) & (sky_data['RA']<300) & (sky_data['DEC']>-15)
+
+    sky_file = os.path.join(output_path, "sky_north.fits")
+    if os.path.exists(sky_file):
+        print("File {} already exists".format(mtl_file))
+    else:
+        sky_data[ii_north].write(sky_file, overwrite=True)
+        print("Wrote output to {}".format(sky_file))
+        
+    sky_file = os.path.join(output_path, "sky_south.fits")
+    if os.path.exists(sky_file):
+        print("File {} already exists".format(mtl_file))
+    else:
+        sky_data[~ii_north].write(sky_file, overwrite=True)
+        print("Wrote output to {}".format(sky_file))
+
+def split_tiles(output_path="./"):
+    # Create output directory
+    os.makedirs(output_path, exist_ok=True)
+    
+    tiles = Table(desimodel.io.load_tiles())
+    
+    ii_bright = tiles['PROGRAM'] == 'BRIGHT'
+    ii_north = (tiles['RA']>85) & (tiles['RA']<300) & (tiles['DEC']>-15)
+    
+    tilefile = os.path.join(output_path, "tiles_bright_north.fits")
+    tiles[ii_bright & ii_north].write(tilefile, overwrite='True')
+    print("Wrote tiles to {}".format(tilefile))
+    
+    tilefile = os.path.join(output_path, "tiles_bright_south.fits")
+    tiles[ii_bright & ~ii_north].write(tilefile, overwrite='True')
+    print("Wrote tiles to {}".format(tilefile))
+    
+    tilefile = os.path.join(output_path, "tiles_dark_gray_north.fits")
+    tiles[~ii_bright & ii_north].write(tilefile, overwrite='True')
+    print("Wrote tiles to {}".format(tilefile))
+    
+    tilefile = os.path.join(output_path, "tiles_dark_gray_south.fits")
+    tiles[~ii_bright & ~ii_north].write(tilefile, overwrite='True')
+    print("Wrote tiles to {}".format(tilefile))
+    
+    ntiles = tiles[ii_bright & ii_north]
+    tilefile = os.path.join(output_path, "tiles_test_bright_north.fits")
+    ntiles[:10].write(tilefile, overwrite='True')
+    print("Wrote tiles to {}".format(tilefile))
+    
+    ntiles = tiles[~ii_bright & ii_north]
+    tilefile = os.path.join(output_path, "tiles_test_dark_north.fits")
+    ntiles[:10].write(tilefile, overwrite='True')
+    print("Wrote tiles to {}".format(tilefile))
+    
+
+        
 targets_path = "targets"
 mtl_files = {}
 mtl_files['dark_gray_north']  = os.path.join(targets_path, "dark_gray_north.fits")
@@ -92,3 +150,18 @@ print(all_exist)
 
 if not all_exist:
     write_initial_mtl_files(output_path=targets_path)
+    
+    
+sky_files = {}
+sky_files['sky_north']  = os.path.join(targets_path, "sky_north.fits")
+sky_files['sky_south']  = os.path.join(targets_path, "sky_south.fits")
+
+all_sky_exist = True
+for k in sky_files.values():
+    all_sky_exist &= os.path.exists(k)
+print(all_sky_exist)
+
+if not all_sky_exist:
+    split_sky(output_path=targets_path)
+    
+split_tiles("footprint")
