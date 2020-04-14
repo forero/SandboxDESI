5ab586de432935fa6c15160bbe1126ced1c2c926
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2020-04-14 15:32:55 -0700

    script

---
+++ DR8/run_all.sh
@@ -0,0 +1,4 @@
+fba_run --targets targets/dark_gray_south.fits --sky targets/sky_south.fits  --footprint footprint/tiles_dark_gray_south.fits --rundate 2019-01-01T00:00:00 --dir fba_dark_gray_south/ --overwrite 
+fba_run --targets targets/dark_gray_north.fits --sky targets/sky_north.fits  --footprint footprint/tiles_dark_gray_north.fits --rundate 2019-01-01T00:00:00 --dir fba_dark_gray_north/ --overwrite 
+fba_run --targets targets/bright_south.fits --sky targets/sky_south.fits --footprint footprint/tiles_bright_south.fits --rundate 2019-01-01T00:00:00 --dir fba_bright_south/ --overwrite
+fba_run --targets targets/bright_north.fits --sky targets/sky_north.fits --footprint footprint/tiles_bright_north.fits --rundate 2019-01-01T00:00:00 --dir fba_bright_north/ --overwrite
