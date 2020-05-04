69623ec70b5c1b9629c38679ab456786fe352846
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2020-05-04 07:53:09 -0700

    joinng targets from desitarget 0.38.0

--- DR8/join_DR8.py
+++ DR8/join_DR8.py
@@ -16,10 +16,16 @@ from desitarget.targetmask import desi_mask, obsconditions
 from collections import Counter
 import subprocess
 
-def write_initial_mtl_files(output_path="./"):
-    
+def write_initial_mtl_files(output_path="./", program='bright'):
+    north_mtl_file = os.path.join(output_path, "{}_north.fits".format(program))
+    south_mtl_file = os.path.join(output_path, "{}_south.fits".format(program))
+
+    if os.path.exists(north_mtl_file) or os.path.exists(south_mtl_file):
+        print("Files {} {} already exist".format(north_mtl_file, south_mtl_file))
+        return
+
     # List all the fits files to read
-    path_to_targets = '/global/cfs/cdirs/desi/target/catalogs/dr8/0.31.1/targets/main/resolve/'
+    path_to_targets = '/global/cfs/projectdirs/desi/target/catalogs/dr8/0.39.0/targets/main/resolve/'+program+'/'
     target_files = glob.glob(os.path.join(path_to_targets, "targets*fits"))
     print('target files to read:', len(target_files))
     target_files.sort()
@@ -38,67 +44,65 @@ def write_initial_mtl_files(output_path="./"):
         print('reading file', i, len(target_files), len(tmp_data))
     full_mtl = desitarget.mtl.make_mtl(target_data, 'DARK|GRAY|BRIGHT')
 
-    ii_mtl_dark = (full_mtl['OBSCONDITIONS'] & obsconditions.DARK)!=0
-    ii_mtl_gray = (full_mtl['OBSCONDITIONS'] & obsconditions.GRAY)!=0
-    ii_mtl_bright = (full_mtl['OBSCONDITIONS'] & obsconditions.BRIGHT)!=0
     ii_north = (full_mtl['RA']>85) & (full_mtl['RA']<300) & (full_mtl['DEC']>-15)
 
     # Create output directory
     os.makedirs(output_path, exist_ok=True)
     
-    print("Writing DARK + GRAY NORTH cap")
-    mtl_file = os.path.join(output_path, "dark_gray_north.fits")
-    if os.path.exists(mtl_file):
-        print("File {} already exists".format(mtl_file))
-    else:
-        full_mtl[(ii_mtl_dark | ii_mtl_gray) & ii_north].write(mtl_file, overwrite=True)
-        print("Wrote output to {}".format(mtl_file))
-    
-    
-    print("Writing BRIGHT NORTH cap")
-    mtl_file = os.path.join(output_path, "bright_north.fits")
-    if os.path.exists(mtl_file):
-        print("File {} already exists".format(mtl_file))
-    else:
-        full_mtl[ii_mtl_bright & ii_north].write(mtl_file, overwrite=True)
-        print("Wrote output to {}".format(mtl_file))
-    
-    print("Writing DARK + GRAY SOUTH cap")
-    mtl_file = os.path.join(output_path, "dark_gray_south.fits")
-    if os.path.exists(mtl_file):
-        print("File {} already exists".format(mtl_file))
-    else:
-        full_mtl[(ii_mtl_dark | ii_mtl_gray) & ~ii_north].write(mtl_file, overwrite=True)
-        print("Wrote output to {}".format(mtl_file))
-    
-    print("Writing BRIGHT SOUTH cap")
-    mtl_file = os.path.join(output_path, "bright_south.fits")
-    if os.path.exists(mtl_file):
-        print("File {} already exists".format(mtl_file))
-    else:
-        full_mtl[ii_mtl_bright & ~ii_north].write(mtl_file, overwrite=True)
-        print("Wrote output to {}".format(mtl_file))
+    print("Writing {} north cap".format(program))
+    full_mtl[ii_north].write(north_mtl_file, overwrite=True)
+    print("Wrote output to {}".format(north_mtl_file))
+        
+    print("Writing {} south cap".format(program))
+    full_mtl[~ii_north].write(south_mtl_file, overwrite=True)
+    print("Wrote output to {}".format(south_mtl_file))
+    return 
 
-def split_sky(output_path="./"):
+def write_sky_files(output_path="./"):
+    # Create output directory
+    os.makedirs(output_path, exist_ok=True)
+    north_sky_file = os.path.join(output_path, "sky_north.fits")
+    south_sky_file = os.path.join(output_path, "sky_south.fits")
+
+    if os.path.exists(north_sky_file) or os.path.exists(south_sky_file):
+        print("Files {} {} already exist".format(north_sky_file, south_sky_file))
+        return
+    
+    
+    # List all the fits files to read
+    path_to_targets = '/global/cfs/projectdirs/desi/target/catalogs/dr8/0.39.0/skies/'
+    target_files = glob.glob(os.path.join(path_to_targets, "skies-*.fits"))
+    print('sky files to read:', len(target_files))
+    target_files.sort()
+    
+    # Read the first file, only the columns that are useful for MTL
+    data = fitsio.FITS(target_files[0], 'r')
+    target_data = data[1].read()
+    data.close()
+    
+    # Read all the other files
+    for i, i_name in enumerate(target_files[1:]): 
+        data = fitsio.FITS(i_name, 'r')
+        tmp_data = data[1].read()
+        target_data = np.hstack((target_data, tmp_data))
+        data.close()
+        print('reading file', i, len(target_files), len(tmp_data))
+
+    target_data = Table(target_data)
+    ii_north = (target_data['RA']>85) & (target_data['RA']<300) & (target_data['DEC']>-15)
+
     # Create output directory
     os.makedirs(output_path, exist_ok=True)
     
-    sky_data = Table.read("/global/cfs/cdirs/desi/target/catalogs/dr8/0.31.0/skies/skies-dr8-0.31.0.fits")
-    ii_north = (sky_data['RA']>85) & (sky_data['RA']<300) & (sky_data['DEC']>-15)
-
-    sky_file = os.path.join(output_path, "sky_north.fits")
-    if os.path.exists(sky_file):
-        print("File {} already exists".format(mtl_file))
-    else:
-        sky_data[ii_north].write(sky_file, overwrite=True)
-        print("Wrote output to {}".format(sky_file))
+    print("Writing north sky cap")
+    target_data[ii_north].write(north_sky_file, overwrite=True)
+    print("Wrote output to {}".format(north_sky_file))
         
-    sky_file = os.path.join(output_path, "sky_south.fits")
-    if os.path.exists(sky_file):
-        print("File {} already exists".format(mtl_file))
-    else:
-        sky_data[~ii_north].write(sky_file, overwrite=True)
-        print("Wrote output to {}".format(sky_file))
+    print("Writing south sky cap")
+    target_data[~ii_north].write(south_sky_file, overwrite=True)
+    print("Wrote output to {}".format(south_sky_file))
+    return 
+
 
 def split_tiles(output_path="./"):
     # Create output directory
@@ -117,11 +121,11 @@ def split_tiles(output_path="./"):
     tiles[ii_bright & ~ii_north].write(tilefile, overwrite='True')
     print("Wrote tiles to {}".format(tilefile))
     
-    tilefile = os.path.join(output_path, "tiles_dark_gray_north.fits")
+    tilefile = os.path.join(output_path, "tiles_dark_north.fits")
     tiles[~ii_bright & ii_north].write(tilefile, overwrite='True')
     print("Wrote tiles to {}".format(tilefile))
     
-    tilefile = os.path.join(output_path, "tiles_dark_gray_south.fits")
+    tilefile = os.path.join(output_path, "tiles_dark_south.fits")
     tiles[~ii_bright & ~ii_north].write(tilefile, overwrite='True')
     print("Wrote tiles to {}".format(tilefile))
     
@@ -138,31 +142,7 @@ def split_tiles(output_path="./"):
 
         
 targets_path = "targets"
-mtl_files = {}
-mtl_files['dark_gray_north']  = os.path.join(targets_path, "dark_gray_north.fits")
-mtl_files['dark_gray_south']  = os.path.join(targets_path, "dark_gray_south.fits")
-mtl_files['bright_north']  = os.path.join(targets_path, "bright_north.fits")
-mtl_files['bright_south'] = os.path.join(targets_path, "bright_south.fits")
-
-all_exist = True
-for k in mtl_files.values():
-    all_exist &= os.path.exists(k)
-print(all_exist)
-
-if not all_exist:
-    write_initial_mtl_files(output_path=targets_path)
-    
-    
-sky_files = {}
-sky_files['sky_north']  = os.path.join(targets_path, "sky_north.fits")
-sky_files['sky_south']  = os.path.join(targets_path, "sky_south.fits")
-
-all_sky_exist = True
-for k in sky_files.values():
-    all_sky_exist &= os.path.exists(k)
-print(all_sky_exist)
-
-if not all_sky_exist:
-    split_sky(output_path=targets_path)
-    
+write_initial_mtl_files(output_path=targets_path, program="dark")
+write_initial_mtl_files(output_path=targets_path, program="bright")
+write_sky_files(output_path=targets_path)
 split_tiles("footprint")
