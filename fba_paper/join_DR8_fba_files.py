9559511608c9afd0011f650b14d2c318e776968c
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2020-10-20 08:10:03 -0700

    join DR8

--- fba_paper/join_DR8_fba_files.py
+++ fba_paper/join_DR8_fba_files.py
@@ -1,17 +1,6 @@
 import join as j
 
-j.join_fba_targets(targets_file="targets/mtl_cut_E_dark_north.fits", 
-                 fba_path="fba_run/mtl_cut_E_tiles_cut_E/", 
-                 summary_filename="summary/fba_summary_mtl_cut_E_tiles_cut_E.fits")
-
-j.join_fba_targets(targets_file="targets/mtl_cut_D_dark_north.fits", 
-                 fba_path="fba_run/mtl_cut_D_tiles_cut_D/", 
-                 summary_filename="summary/fba_summary_mtl_cut_D_tiles_cut_D.fits")
-
-j.join_fba_targets(targets_file="targets/mtl_cut_C_dark_north.fits", 
-                 fba_path="fba_run/mtl_cut_C_tiles_cut_C/", 
-                 summary_filename="summary/fba_summary_mtl_cut_C_tiles_cut_C.fits")
-
-j.join_fba_targets(targets_file="targets/mtl_cut_B_dark_north.fits", 
-                 fba_path="fba_run/mtl_cut_B_tiles_cut_B/", 
-                 summary_filename="summary/fba_summary_mtl_cut_B_tiles_cut_B.fits")
+for i in ["A", "B", "C", "D", "E"]:
+    j.join_fba_targets(targets_file="targets/mtl_cut_{}_dark_south.fits".format(i), 
+                       fba_path="fba_run/mtl_dark_south_cut_{}_tiles_cut_{}/".format(i,i), 
+                       summary_filename="summary/fba_summary_mtl_dark_south_cut_{}_tiles_cut_{}.fits".format(i,i))
