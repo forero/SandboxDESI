43f8f6d46a67803a9dba5c094b22db0a44510cd7
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2020-11-12 16:59:43 -0800

    updated script

--- fba_format/convert.py
+++ fba_format/convert.py
@@ -16,13 +16,18 @@ print('writing file {}'.format(filenameout))
 filein =  fitsio.FITS(filenamein,'r')
 fileout = fitsio.FITS(filenameout, 'rw')
 
-extnames = ['PRIMARY','FIBERASSIGN','GFA_TARGETS','TARGETS','POTENTIAL_ASSIGNMENTS']
+extnames = ['PRIMARY','FIBERASSIGN','SKY_MONITOR','GFA_TARGETS','TARGETS','POTENTIAL_ASSIGNMENTS']
 
 columns = {'FIBERASSIGN':
           ['FIBER', 'TARGETID', 'LOCATION', 'FIBERSTATUS', 
            'LAMBDA_REF' , 'PETAL_LOC', 'TARGET_RA', 
            'TARGET_DEC', 'FA_TARGET' , 'FA_TYPE', 
-           'FIBERASSIGN_X', 'FIBERASSIGN_Y'], 
+           'FIBERASSIGN_X', 'FIBERASSIGN_Y', 'DEVICE_LOC'], 
+           'SKY_MONITOR':
+           ['FIBER', 'TARGETID', 'LOCATION', 'FIBERSTATUS', 
+           'PETAL_LOC', 'TARGET_RA', 
+           'TARGET_DEC', 'FA_TARGET' , 'FA_TYPE', 
+           'FIBERASSIGN_X', 'FIBERASSIGN_Y', 'DEVICE_LOC'], 
           'TARGETS':
           ['TARGETID', 'RA', 'DEC' , 
            'FA_TARGET', 'FA_TYPE' , 'PRIORITY' , 
