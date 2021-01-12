36bc516775d6b99b13e5b3c2d2a93a49baa65950
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2021-01-11 20:27:27 -0800

    updated py scripts

--- secondary/generate_fba_scnd_runs.py
+++ secondary/generate_fba_scnd_runs.py
@@ -2,7 +2,7 @@ import numpy as np
 from astropy.table import Table
 
 def print_run(flavor='science', tilera=0.0, tiledec=0.0, tileid=1, 
-              scnd=False, priority='custom'):
+              scnd=False, priority='custom', fiber='restricted'):
  
     sv1_exec = 'python fba_scnd_sv1'
     if scnd:
@@ -10,7 +10,12 @@ def print_run(flavor='science', tilera=0.0, tiledec=0.0, tileid=1,
     else:
         scnd_str = 'false'
         
-    run =  '{} --dr dr9 --dtver 0.47.0 --rundate 2020-03-06T00:00:00 '.format(sv1_exec)
+    if fiber=='restricted':
+        rundate = '2020-03-06T00:00:00'
+    elif fiber=='unrestricted':
+        rundate = '2020-01-01T00:00:00'
+        
+    run =  '{} --dr dr9 --dtver 0.47.0 --rundate {} '.format(sv1_exec, rundate)
     run += ' --tilera {} ' .format(tilera)
     run += ' --tiledec {} ' .format(tiledec)
     run += ' --tileid {} '.format(tileid)
@@ -18,7 +23,7 @@ def print_run(flavor='science', tilera=0.0, tiledec=0.0, tileid=1,
     run += ' --priority {}'.format(priority)
     if scnd:
         run += ' --scnd '
-    run += ' --outdir ./tiles_sv1_scnd/{}_{}_scnd_{}_{:06d} '.format(flavor, priority, scnd_str, tileid)
+    run += ' --outdir ./tiles_sv1_scnd/{}_{}_scnd_{}_fiber_{}_{:06d} '.format(flavor, priority, scnd_str, fiber, tileid)
     return run
 
 data = Table.read("sv1-tiles.fits")
@@ -32,6 +37,6 @@ for i in range(len(tileids)):
     tileid = tileids[i]
     for flavor in flavors:
         for s in scnd:
-            run = print_run(flavor=flavor, tilera=ra[i], tiledec=dec[i], tileid=tileid, scnd=s, priority='custom')
+            run = print_run(flavor=flavor, tilera=ra[i], tiledec=dec[i], tileid=tileid, scnd=s, priority='custom', fiber='restricted')
             print(run)
 
--- secondary/summary_stats.py
+++ secondary/summary_stats.py
@@ -9,11 +9,12 @@ import sys
 #flavors ["sv1elg", "sv1elgqso","sv1lrgqso","sv1bgsmws"]
 flavor = sys.argv[1]
 priority = sys.argv[2]
+fiber = sys.argv[3]
 print("flavor ", flavor)
 print("priority", priority)
 
 scnd = "true"
-fba_files = glob.glob("tiles_sv1_scnd/{}_{}_scnd_{}_*/fiberassign-*.fits.gz".format(flavor, priority, scnd))
+fba_files = glob.glob("tiles_sv1_scnd/{}_{}_scnd_{}_fiber_{}*/fiberassign-*.fits.gz".format(flavor, priority, scnd, fiber))
 fba_files.sort()
 print(len(fba_files))
 
@@ -22,10 +23,21 @@ names = [n for n in scnd_mask.names()]
 
 for n in names:
     scnd_targets[n] = {}
+    scnd_targets[n]['n_intile_main'] = []
+    scnd_targets[n]['n_intile_scnd'] = []
     scnd_targets[n]['n_avail'] = []
     scnd_targets[n]['n_assign'] = []
     
 for i in range(len(fba_files)):
+    scnd_target_files = fba_files[i].replace('fiberassign-', '/')
+    scnd_target_files = scnd_target_files.replace('.fits.gz', '-scnd.fits')
+    scnd_all_targets = Table.read(scnd_target_files)
+    masks_scnd_tile = scnd_all_targets['SV1_SCND_TARGET']
+    
+    main_target_files = scnd_target_files.replace('scnd.fits', 'targ.fits')
+    main_all_targets  = Table.read(main_target_files)
+    masks_main_tile = main_all_targets['SV1_SCND_TARGET']
+    
     flavor = fba_files[i].split('/')[1].split('_')[0]
     scnd = fba_files[i].split('/')[1].split('_')[3]
     targetid = fba_files[i].split('/')[1].split('_')[-1]
@@ -46,22 +58,31 @@ for i in range(len(fba_files)):
     for n in names:
         ii_avail = (masks_potential & scnd_mask.mask(n))!=0
         ii_assigned = (masks_assigned & scnd_mask.mask(n))!=0
+        ii_intile_scnd = (masks_scnd_tile & scnd_mask.mask(n))!=0
+        ii_intile_main = (masks_main_tile & scnd_mask.mask(n))!=0
+        n_intile_main = np.count_nonzero(ii_intile_main)
+        n_intile_scnd = np.count_nonzero(ii_intile_scnd)
         n_avail = np.count_nonzero(ii_avail)
         n_assigned = np.count_nonzero(ii_assigned)
+        scnd_targets[n]['n_intile_main'].append(n_intile_main)
+        scnd_targets[n]['n_intile_scnd'].append(n_intile_scnd)
         scnd_targets[n]['n_avail'].append(n_avail)
         scnd_targets[n]['n_assign'].append(n_assigned)
         #print(n, n_avail, n_assigned)
     #print(flavor, targetid, n_scnd)
 
-filename = 'scnd_summary_{}_{}.txt'.format(flavor, priority)
+filename = 'scnd_summary_{}_{}_{}.txt'.format(flavor, priority, fiber)
 f = open(filename, 'w')
-f.write('FAFLAVOR=`{}`, PRIORITY=`{}`\n'.format(flavor, priority))
-f.write('||= SCND_MASK =||=N_AVAIL =||=N_ASSIGNED =||=Eff =||\n')
+f.write('FAFLAVOR=`{}`, PRIORITY=`{}`, FIBER=`{}`\n'.format(flavor, priority, fiber))
+f.write('||= SCND_MASK =||=N_INTILE_MAIN =||= N_INTILE_SCND =||=N_AVAIL =||=N_ASSIGNED =||=Eff (N_ASSIGNED/N_INTILE)=||\n')
 for n in names:
     if n!='VETO':
-        f.write('|| {} || {} || {} || {:.4f} ||\n'.format(n, 
+        n_total = np.sum(scnd_targets[n]['n_intile_main']) + np.sum(scnd_targets[n]['n_intile_scnd'])
+        f.write('|| {} || {} || {} || {} || {} || {:.4f} ||\n'.format(n, 
+                                            np.sum(scnd_targets[n]['n_intile_main']),
+                                            np.sum(scnd_targets[n]['n_intile_scnd']),
                                              np.sum(scnd_targets[n]['n_avail']), 
                                              np.sum(scnd_targets[n]['n_assign']),
-                                            np.sum(scnd_targets[n]['n_assign'])/(1+np.sum(scnd_targets[n]['n_avail']))))
+                                            np.sum(scnd_targets[n]['n_assign'])/(1+n_total)))
 
 f.close()
