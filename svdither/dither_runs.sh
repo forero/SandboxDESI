478ca5c1090f068b074226e0d6dbe57b9f116fbc
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2021-02-04 07:45:04 -0800

    commands

--- svdither/dither_runs.sh
+++ svdither/dither_runs.sh
@@ -1,3 +1,5 @@
-python fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2021-01-20T00:00:00 --seed 77  --tilera 188.0  --tiledec 30.0 --tileid 82000  --faflavor dithprec  --outdir ./082000
-python fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2021-01-20T00:00:00 --seed 77  --tilera 111.64  --tiledec 30.0 --tileid 82100  --faflavor dithprec  --outdir ./082100
-python fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2021-01-20T00:00:00 --seed 77  --tilera 111.64  --tiledec 30.0 --tileid 82200  --faflavor dithfocus --outdir ./082200
+python ~/fiberassign/bin/fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2021-01-20T00:00:00 --seed 77  --tilera 188.0  --tiledec 30.0 --tileid 80743  --faflavor dithprec  --outdir ./080743
+python ~/fiberassign/bin/fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2021-01-20T00:00:00 --seed 77  --tilera 111.64  --tiledec 30.0 --tileid 80756  --faflavor dithprec  --outdir ./080756
+python ~/fiberassign/bin/fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2021-01-20T00:00:00 --seed 77  --tilera 111.64  --tiledec 30.0 --tileid 80769  --faflavor dithfocus --outdir ./080769
+rm -v $DESI_ROOT/users/forero/tiles_dither_february/*
+cp -v ./080*/fiber* $DESI_ROOT/users/forero/tiles_dither_february/
