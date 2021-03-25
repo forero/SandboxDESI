f7a72fa179b59fa7f2ec5b37baabebfe490fab5e
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2021-03-24 19:44:42 -0700

    updated sh

---
+++ bgstest/fiber_runs.sh
@@ -0,0 +1,14 @@
+#fba_run --targets targets/mtl_bright_main_onepct.fits --sky targets/sky_onepct.fits --footprint tiles/tiles_one_pct_bright.fits --rundate 2020-01-01T00:00:00 --dir fba_output_main_bright/ --overwrite
+
+#fba_run --targets targets/mtl_bright_sv1_onepct.fits --sky targets/sky_onepct.fits --footprint tiles/tiles_one_pct_bright.fits --rundate 2020-01-01T00:00:00 --dir fba_output_sv1_bright/ --overwrite
+
+
+#fba_run --targets targets/mtl_bright_main_onepct.fits --sky targets/sky_onepct.fits --footprint tiles/tiles_one_pct_bright.fits --rundate 2021-03-17T23:20:01 --dir fba_output_main_bright_updated_pos/ --overwrite
+
+#fba_run --targets targets/mtl_bright_sv1_onepct.fits --sky targets/sky_onepct.fits --footprint tiles/tiles_one_pct_bright.fits --rundate 2021-03-17T23:20:01 --dir fba_output_sv1_bright_updated_pos/ --overwrite
+
+#fba_run --targets targets/mtl_bright_newbgs_sv1_onepct.fits --sky targets/sky_onepct.fits --footprint tiles/tiles_one_pct_bright.fits --rundate 2020-01-01T00:00:00 --dir fba_output_newbgs_sv1_bright/ --overwrite
+
+#fba_run --targets targets/mtl_bright_newbgs_sv1_onepct.fits --sky targets/sky_onepct.fits --footprint tiles/tiles_one_pct_bright.fits --rundate 2021-03-17T23:20:01 --dir fba_output_newbgs_sv1_updated_pos/ --overwrite
+
+fba_run --targets targets/mtl_bright_newbgs_david_sv1_onepct.fits --sky targets/sky_onepct.fits --footprint tiles/tiles_one_pct_bright.fits --rundate 2021-03-17T23:20:01 --dir fba_output_newbgs_david_sv1_updated_pos/ --overwrite
