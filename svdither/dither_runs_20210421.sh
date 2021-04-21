91d33e5b957fc87e5cf1b37c7219301735017fd5
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2021-04-21 04:17:10 -0700

    dither runs 20210421

---
+++ svdither/dither_runs_20210421.sh
@@ -0,0 +1,16 @@
+python fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2021-04-21T10:00:00 --seed 77  --tilera 190.0  --tiledec 49.5 --tileid 82000  --faflavor dithprec  --outdir 20210421 &
+
+python fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2021-04-21T10:00:00 --seed 77  --tilera 220.2  --tiledec 47.8 --tileid 82013  --faflavor dithprec  --outdir 20210421 &
+
+python fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2021-04-21T21:00:00 --seed 77  --tilera 251.2  --tiledec 50.4 --tileid 82026  --faflavor dithprec  --outdir 20210421 &
+
+python fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2021-04-21T21:00:00 --seed 77  --tilera 279.0  --tiledec 50.0 --tileid 82039  --faflavor dithprec  --outdir 20210421 &
+
+python fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2021-04-21T21:00:00 --seed 77  --tilera 190.0  --tiledec 49.5 --tileid 82052  --faflavor dithlost  --outdir 20210421  &
+
+python fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2021-04-21T21:00:00 --seed 77  --tilera 220.2  --tiledec 47.8 --tileid 82065  --faflavor dithlost  --outdir 20210421 &
+
+python fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2021-04-21T21:00:00 --seed 77  --tilera 251.2  --tiledec 50.4 --tileid 82078  --faflavor dithlost  --outdir 20210421 &
+
+python fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2021-04-21T21:00:00 --seed 77  --tilera 279.0  --tiledec 50.0 --tileid 82091  --faflavor dithlost  --outdir 20210421 &
+
