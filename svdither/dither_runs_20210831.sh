5772244d4c3e370622b05d9af71ba2bb3f4267ee
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2021-09-07 10:18:39 -0700

    new sv dithers

---
+++ svdither/dither_runs_20210831.sh
@@ -0,0 +1,12 @@
+fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2021-08-31T21:00:00 --seed 77  --tilera 278.0  --tiledec 36.5 --tileid 82104  --faflavor dithprec  --outdir 20210831
+fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2021-08-31T21:00:00 --seed 77  --tilera 281.75  --tiledec 46.32 --tileid 82117  --faflavor dithprec  --outdir 20210831
+fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2021-08-31T21:00:00 --seed 77  --tilera 336.0  --tiledec 30.0 --tileid 82130  --faflavor dithprec  --outdir 20210831
+fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2021-08-31T21:00:00 --seed 77  --tilera 1.4  --tiledec 31.2 --tileid 82143  --faflavor dithprec  --outdir 20210831
+fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2021-08-31T21:00:00 --seed 77  --tilera 31.0  --tiledec 31.0 --tileid 82156  --faflavor dithprec  --outdir 20210831
+
+fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2021-08-31T21:00:00 --seed 77  --tilera 278.0  --tiledec 36.5 --tileid 82169  --faflavor dithfocus  --outdir 20210831
+fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2021-08-31T21:00:00 --seed 77  --tilera 281.75  --tiledec 46.32 --tileid 82182  --faflavor dithfocus  --outdir 20210831
+fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2021-08-31T21:00:00 --seed 77  --tilera 336.0  --tiledec 30.0 --tileid 82195  --faflavor dithfocus  --outdir 20210831
+fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2021-08-31T21:00:00 --seed 77  --tilera 1.4  --tiledec 31.2 --tileid 82208  --faflavor dithfocus  --outdir 20210831
+fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2021-08-31T21:00:00 --seed 77  --tilera 31.0  --tiledec 31.0 --tileid 82221  --faflavor dithfocus  --outdir 20210831
+
