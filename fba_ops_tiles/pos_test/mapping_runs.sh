82212e110a13123d101596b504e32db5e7fa4d5a
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2021-09-08 13:17:09 -0700

    test shell script

---
+++ fba_ops_tiles/pos_test/mapping_runs.sh
@@ -0,0 +1,2 @@
+python my_fba_cmx --dr=dr9 --dtver=0.49.0 --rundate=2021-09-07T21:00:00 --seed=77 --faflavor=dithlost --outdir=./ --tilera=343.0 --tiledec=30.0 --tileid=89000
+python unique_targets.py 
