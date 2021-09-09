c9dfc721a05673c4a1a9388a04351060e5ac01a8
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2021-09-09 15:24:34 -0700

    unique

--- fba_ops_tiles/positioner_mapping/unique_targets.py
+++ fba_ops_tiles/positioner_mapping/unique_targets.py
@@ -2,9 +2,9 @@ import numpy as np
 import fitsio
 from collections import Counter
 
-target_filename = "089000-targ.fits"
+target_filename = "090000-targ.fits"
 data_main = fitsio.read(target_filename)
-fba_potential = fitsio.read("fiberassign-089000.fits", ext="POTENTIAL_ASSIGNMENTS")
+fba_potential = fitsio.read("fiberassign-090000.fits", ext="POTENTIAL_ASSIGNMENTS")
 
 # only include targets that were potential targets to a single fiber
 count_potential = Counter(fba_potential['TARGETID'])
