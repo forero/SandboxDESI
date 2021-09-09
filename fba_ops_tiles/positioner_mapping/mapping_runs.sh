1640ab9b938499009504aced21fce9cc208adc56
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2021-09-09 15:24:44 -0700

    shell script

--- fba_ops_tiles/positioner_mapping/mapping_runs.sh
+++ fba_ops_tiles/positioner_mapping/mapping_runs.sh
@@ -1,2 +1,47 @@
-python my_fba_cmx --dr=dr9 --dtver=0.49.0 --rundate=2021-09-07T21:00:00 --seed=77 --faflavor=dithlost --outdir=./ --tilera=343.0 --tiledec=30.0 --tileid=89000
+fba_cmx --dr=dr9 --dtver=0.49.0 --rundate=2021-09-07T21:00:00 --seed=77 --faflavor=dithlost --outdir=./ --tilera=343.0 --tiledec=30.0 --tileid=90000
+
 python unique_targets.py 
+
+fiberassign --mtl 090000-unique_targ.fits --sky 090000-sky.fits --gfafile 090000-gfa.fits --nstarpetal 0 --footprint 090000-tiles.fits --overwrite 
+
+python multiple_target_files.py
+
+fiberassign --mtl 090000-unique_unique_targ_m_00.fits --sky 090000-sky.fits --gfafile 090000-gfa.fits --nstarpetal 0 --footprint 090000-tiles.fits --overwrite
+mv fiberassign-090000.fits fiberassign-082248.fits
+gzip  fiberassign-082248.fits
+
+fiberassign --mtl 090000-unique_unique_targ_m_01.fits --sky 090000-sky.fits --gfafile 090000-gfa.fits --nstarpetal 0 --footprint 090000-tiles.fits --overwrite
+mv fiberassign-090000.fits fiberassign-082249.fits
+gzip fiberassign-082249.fits
+
+fiberassign --mtl 090000-unique_unique_targ_m_02.fits --sky 090000-sky.fits --gfafile 090000-gfa.fits --nstarpetal 0 --footprint 090000-tiles.fits --overwrite
+mv fiberassign-090000.fits fiberassign-082250.fits
+gzip fiberassign-082250.fits
+
+fiberassign --mtl 090000-unique_unique_targ_m_03.fits --sky 090000-sky.fits --gfafile 090000-gfa.fits --nstarpetal 0 --footprint 090000-tiles.fits --overwrite
+mv fiberassign-090000.fits fiberassign-082251.fits
+gzip fiberassign-082251.fits
+
+fiberassign --mtl 090000-unique_unique_targ_m_04.fits --sky 090000-sky.fits --gfafile 090000-gfa.fits --nstarpetal 0 --footprint 090000-tiles.fits --overwrite
+mv fiberassign-090000.fits fiberassign-082252.fits
+gzip fiberassign-082252.fits
+
+fiberassign --mtl 090000-unique_unique_targ_m_05.fits --sky 090000-sky.fits --gfafile 090000-gfa.fits --nstarpetal 0 --footprint 090000-tiles.fits --overwrite
+mv fiberassign-090000.fits fiberassign-082253.fits 
+gzip fiberassign-082253.fits 
+
+fiberassign --mtl 090000-unique_unique_targ_m_06.fits --sky 090000-sky.fits --gfafile 090000-gfa.fits --nstarpetal 0 --footprint 090000-tiles.fits --overwrite
+mv fiberassign-090000.fits fiberassign-082254.fits 
+gzip fiberassign-082254.fits
+
+fiberassign --mtl 090000-unique_unique_targ_m_07.fits --sky 090000-sky.fits --gfafile 090000-gfa.fits --nstarpetal 0 --footprint 090000-tiles.fits --overwrite
+mv fiberassign-090000.fits fiberassign-082255.fits 
+gzip fiberassign-082255.fits 
+
+fiberassign --mtl 090000-unique_unique_targ_m_08.fits --sky 090000-sky.fits --gfafile 090000-gfa.fits --nstarpetal 0 --footprint 090000-tiles.fits --overwrite
+mv fiberassign-090000.fits fiberassign-082256.fits 
+gzip fiberassign-082256.fits
+
+fiberassign --mtl 090000-unique_unique_targ_m_09.fits --sky 090000-sky.fits --gfafile 090000-gfa.fits --nstarpetal 0 --footprint 090000-tiles.fits --overwrite
+mv fiberassign-090000.fits fiberassign-082257.fits
+gzip fiberassign-082257.fits
