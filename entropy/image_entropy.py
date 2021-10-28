215943c7327c03e62b2ead655467506d1cc754e7
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2021-10-28 14:11:57 -0700

    image entropy python

--- entropy/image_entropy.py
+++ entropy/image_entropy.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from itertools import combinations, permutations
 
-def compute_probability_distribution(data):
+
+def compute_probability_distribution_1D(data):
     s = np.shape(data)
     n_side = 2
     n_x = s[0]-1
@@ -14,6 +15,39 @@ def compute_probability_distribution(data):
     for i in range(0,n_x):
         for j in range(0,n_y):
             tmp = data[i : i+n_side, j:j+n_side].flatten()
+            sorted_ids = np.int_(np.argsort(tmp))
+            s = ''.join(str(j) for j in sorted_ids)
+            try:
+                sorts[s] += 1
+            except:
+                sorts[s] = 1
+    
+    # return the probabilities in the order provided by the permutations routine
+    a = '0123'
+    perms = permutations(a, 4)
+    sorted_perms = []
+    for p in perms:
+        sorted_perms.append(''.join(i for i in p))
+        
+    values = []
+    for p in sorted_perms:
+        values.append(sorts[p])
+    values = np.array(values)
+    values = values/np.sum(values)
+
+def compute_probability_distribution_2D(data, n_stride=2):
+    s = np.shape(data)
+    n_side = 2
+    
+    n_x = s[0]//n_stride -1
+    n_y = s[1]//n_stride -1
+    print("computation over {} locations ".format(n_x*n_y))
+
+    
+    sorts = {}
+    for i in range(n_x):
+        for j in range(n_y):
+            tmp = data[(i*n_stride) : (i*n_stride)+n_side, (j*n_stride):(j*n_stride)+n_side].flatten()
             sorted_ids = np.int_(np.argsort(tmp))
             s = ''.join(str(j) for j in sorted_ids)
             try:
