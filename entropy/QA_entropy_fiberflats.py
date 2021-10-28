b44bfbed754c6aa8812201bc462d73d0c41a0387
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2021-10-28 14:11:31 -0700

    QA entropy fiberflats

--- entropy/QA_entropy_fiberflats.py
+++ entropy/QA_entropy_fiberflats.py
@@ -13,17 +13,30 @@ def read_fflat(filename):
     return fflat_data
 
 data_path = "/global/cfs/cdirs/desi/spectro/redux/daily/calibnight/"
-fiberflats = glob.glob(os.path.join(data_path, "2021*/fiberflatnight-z0-*.fits"))
+fiberflats = glob.glob(os.path.join(data_path, "20211*/fiberflatnight-z0-*.fits"))
 fiberflats.sort()
 
-for camera in ['b', 'r', 'z']:
-    for petal in range(2):
-        camera_petal = camera+str(petal)
-        
-        for f in fiberflats[:2]:
+
+outfile = open("calibnight_entropy.csv","w")
+outfile.write("NIGHT,CAMERA,PETAL,ENTROPY\n")
+outfile.close()
+
+for f in fiberflats:
+    date = f.split('/')[-2]
+    for petal in range(10):
+        for camera in ['b', 'r', 'z']:
+            camera_petal = camera+str(petal)
             fflat = f.replace('z0', camera_petal)
             print(fflat)
-            fflat_data = read_fflat(fflat)
-            pdistro = ient.compute_probability_distribution(fflat_data)
-            entropy = ient.compute_entropy(pdistro)
-            print(entropy)
+            try:
+                fflat_data = read_fflat(fflat)
+                pdistro = ient.compute_probability_distribution_2D(fflat_data, n_stride=10)
+                entropy = ient.compute_entropy(pdistro)
+            
+                out = "{},{},{},{}\n".format(date, camera, petal, entropy)
+                print(out)
+                outfile = open("calibnight_entropy.csv","a")
+                outfile.write(out)
+                outfile.close()
+            except:
+                pass
