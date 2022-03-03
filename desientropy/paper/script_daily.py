1fecf3d5e2f309849af605e124cf800fff52721e
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2022-03-03 12:10:34 -0800

    daily

--- desientropy/paper/script_daily.py
+++ desientropy/paper/script_daily.py
@@ -1,3 +1,8 @@
 import desientropy.redrock as redrock_entropy
 
+
 redrock_entropy.summary_release_entropy("daily")
+#redrock_entropy.summary_release_entropy("daily", lastnight=20220301)
+
+#for i in range(1,29):
+#	redrock_entropy.summary_release_entropy("daily", lastnight=20220200+i)
