af26f506aa754dfc695dccbfc6df8bce28b2507d
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2022-03-08 09:38:52 -0800

    first week of march

--- desientropy/paper/script_daily.py
+++ desientropy/paper/script_daily.py
@@ -1,8 +1,8 @@
 import desientropy.redrock as redrock_entropy
 
 
-redrock_entropy.summary_release_entropy("daily")
+#redrock_entropy.summary_release_entropy("daily")
 #redrock_entropy.summary_release_entropy("daily", lastnight=20220301)
 
-#for i in range(1,29):
-#	redrock_entropy.summary_release_entropy("daily", lastnight=20220200+i)
+for i in range(1,7):
+	redrock_entropy.summary_release_entropy("daily", lastnight=20220300+i)
