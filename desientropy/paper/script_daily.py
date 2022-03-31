c5af6ef450c876a9cd9ccc6f8e1f8caea1faad72
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2022-03-31 12:22:06 -0700

    testing march data

--- desientropy/paper/script_daily.py
+++ desientropy/paper/script_daily.py
@@ -4,5 +4,5 @@ import desientropy.redrock as redrock_entropy
 #redrock_entropy.summary_release_entropy("daily")
 #redrock_entropy.summary_release_entropy("daily", lastnight=20220301)
 
-for i in range(1,7):
+for i in range(1,31):
 	redrock_entropy.summary_release_entropy("daily", lastnight=20220300+i)
--- desientropy/paper/test_sky_sframe.py
+++ desientropy/paper/test_sky_sframe.py
@@ -1,4 +1,4 @@
 import desientropy.sky_sframe
 
-desientropy.sky_sframe.summary_entropy_night("daily", 20220305)
+desientropy.sky_sframe.summary_entropy_night("daily", 20220329)
 #desientropy.sky_sframe.summary_entropy_expid("daily", 20220305, 125018)
