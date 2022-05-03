7198170f4920221ccdfd27936c3104724adbadc5
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2022-05-03 11:17:54 -0700

    full raw exp computation for a whole night

--- desientropy/explore_calibs/raw_exp.py
+++ desientropy/explore_calibs/raw_exp.py
@@ -4,6 +4,7 @@ import fitsio
 import os
 import glob
 import desientropy.compute
+import pandas as pd
 
 def list_raw_exps(date):
     data_path = '/global/cfs/cdirs/desi/spectro/data/'
@@ -12,6 +13,7 @@ def list_raw_exps(date):
     return desi_raw_exps
 
 def read_raw_exp(desi_exp_filename):
+    print('reading ', desi_exp_filename)
     fits=fitsio.FITS(desi_exp_filename)
     n_hdu = len(fits)
     a = fits[1]
@@ -25,12 +27,10 @@ def read_raw_exp(desi_exp_filename):
     for i in range(2,4):#n_hdu-1):
         a = fits[i]
         ext_name = a.get_extname()
-        print(ext_name)
         raw_data[ext_name] = a.read()
-        print(np.shape(raw_data[ext_name]))
     return {'DATA':raw_data, 'OBSTYPE':obstype, 'PROGRAM':program, 'EXPID':expid, 'NIGHT':night}
 
-def new_data_amp(data_amp, tau=20):
+def new_data_amp(data_amp, tau=30):
     n_x = np.shape(data_amp)[0]
     n_y = np.shape(data_amp)[1]
     new_n_x = n_x//tau
@@ -69,17 +69,25 @@ def compute_raw_exp_entropy(raw_exp_data):
         entropy = list(range(len(amps)))
         for i in range(len(amps)):
             entropy[i] = desientropy.compute.entropy_2d(data_amp[i])
-
             
         for i in range(len(amps)):
             summary['SPECTRO'].append(spectro)
             summary['AMP'].append(amps[i])
             summary['H'].append(entropy[i])
-            for j in ['EXPID', 'PROGRAM', 'OBSTYPE']:
+            for j in ['EXPID', 'PROGRAM', 'OBSTYPE', 'NIGHT']:
                 summary[j].append(raw_exp_data[j])
-
-
+    #print(summary)
+    entropy_df = pd.DataFrame.from_dict(summary)
+    filename = 'entropy_raw_exp_{}_{:08d}.csv'.format(raw_exp_data['NIGHT'], raw_exp_data['EXPID'])    
+    entropy_df.to_csv(filename)
+    return 
         
-raw_exp_files = list_raw_exps(20220502)
-raw_exp_data = read_raw_exp(raw_exp_files[1])
-compute_raw_exp_entropy(raw_exp_data)
+def summary_entropy_night(date):
+    raw_exp_files = list_raw_exps(date)
+    n = len(raw_exp_files)
+    for i in range(n):
+        raw_exp_data = read_raw_exp(raw_exp_files[i])
+        compute_raw_exp_entropy(raw_exp_data)
+        del raw_exp_data
+        
+summary_entropy_night(20220502)
