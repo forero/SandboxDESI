5ec3b5fc0228bddaa0d5ef66452de34898c8bba5
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2022-05-17 11:43:36 -0700

    dither runs

--- svdither/dither_runs_20220517.sh
+++ svdither/dither_runs_20220517.sh
@@ -2,15 +2,15 @@ fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2022-05-17T10:00:00 --seed 77  --tiler
 
 fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2022-05-17T10:00:00 --seed 77  --tilera 220.2  --tiledec 47.8 --tileid 82647  --faflavor dithprec  --outdir 20220517 &
 
-fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2022-05-17T21:00:00 --seed 77  --tilera 251.2  --tiledec 50.4 --tileid 82660  --faflavor dithprec  --outdir 20220517 &
+fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2022-05-17T10:00:00 --seed 77  --tilera 251.2  --tiledec 50.4 --tileid 82660  --faflavor dithprec  --outdir 20220517 &
 
-fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2022-05-17T21:00:00 --seed 77  --tilera 279.0  --tiledec 50.0 --tileid 82673  --faflavor dithprec  --outdir 20220517 &
+fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2022-05-17T10:00:00 --seed 77  --tilera 279.0  --tiledec 50.0 --tileid 82673  --faflavor dithprec  --outdir 20220517 &
 
-fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2022-05-17T21:00:00 --seed 77  --tilera 190.0  --tiledec 49.5 --tileid 82686  --faflavor dithlost  --outdir 20220517  &
+fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2022-05-17T10:00:00 --seed 77  --tilera 190.0  --tiledec 49.5 --tileid 82686  --faflavor dithfocus  --outdir 20220517  &
 
-fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2022-05-17T21:00:00 --seed 77  --tilera 220.2  --tiledec 47.8 --tileid 82699  --faflavor dithlost  --outdir 20220517 &
+fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2022-05-17T10:00:00 --seed 77  --tilera 220.2  --tiledec 47.8 --tileid 82699  --faflavor dithfocus  --outdir 20220517 &
 
-fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2022-05-17T21:00:00 --seed 77  --tilera 251.2  --tiledec 50.4 --tileid 82712  --faflavor dithlost  --outdir 20220517 &
+fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2022-05-17T10:00:00 --seed 77  --tilera 251.2  --tiledec 50.4 --tileid 82712  --faflavor dithfocus  --outdir 20220517 &
 
-fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2022-05-17T21:00:00 --seed 77  --tilera 279.0  --tiledec 50.0 --tileid 82725  --faflavor dithlost  --outdir 20220517 &
+fba_cmx --dr dr9 --dtver 0.49.0 --rundate 2022-05-17T10:00:00 --seed 77  --tilera 279.0  --tiledec 50.0 --tileid 82725  --faflavor dithfocus  --outdir 20220517 &
 
