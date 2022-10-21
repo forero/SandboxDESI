e6ae26ad89412a77b63d16001103bda7f1e86434
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2022-10-21 12:36:44 -0700

    daily after restart

--- desientropy/daily/script_daily.py
+++ desientropy/daily/script_daily.py
@@ -2,7 +2,8 @@ import desientropy.redrock as redrock_entrop
 import desientropy.raw_exp
 import desientropy.sky_sframe
 
-for i in range(0,10):
-	desientropy.redrock.summary_release_entropy("daily", lastnight=20220500+i)
-        desientropy.sky_sframe.summary_entropy_night("daily", 20220500+i, sample_lambda=True)
-        desientropy.raw_exp.summary_entropy_night(20220500 + i)
+for month in [9,10]:
+    for day in range(0,31):
+        desientropy.redrock.summary_release_entropy("daily", lastnight=2022*10000 + month*100 + day)
+        #desientropy.sky_sframe.summary_entropy_night("daily", 20220500+i, sample_lambda=True)
+        #desientropy.raw_exp.summary_entropy_night(20220500 + i)
